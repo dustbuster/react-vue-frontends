fe829a2010303b4fffd25cfd9b3aa8f30219d8b8
dustbuster <me@dustinhorning.net>
2024-09-23 08:27:48 -0400

    DEV: Going thru NEXT tutorial to understand how it works.
    In this lesson, we added custom fonts and css.
    Introducing tailwind.

---
+++ nextjs-dashboard/app/ui/fonts.ts
@@ -0,0 +1,13 @@
+import { Inter, Lusitana, Roboto_Mono } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+
+export const roboto_mono = Roboto_Mono({
+  subsets: ['latin'],
+  display: 'swap',
+});
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: '400',
+});
